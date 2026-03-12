77b84903ea4125f1993e0290ba95475e2ffac7f7
Gaf Dev <gayratovazizjon33@gmail.com>
2026-03-12 14:27:35 +0500

    fixed problems

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Gaf from './components/Gaf'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
-      
+      <Gaf/>
+
     </div>
   )
 }
